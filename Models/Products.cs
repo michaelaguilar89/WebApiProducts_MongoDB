ca9190a4f0a22d9568fb5a63ca079a09d40c5be8
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-05-27 15:03:58 -0600

    adding the product model

---
+++ Models/Products.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiProducts_MongoDB.Models
+{
+    public class Products
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Price { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        public Object? Urls { get; set; }
+    }
+}
