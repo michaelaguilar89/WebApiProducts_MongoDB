da9ebf0d16abc9f02bd643c146c177861e4ac782
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-05-27 15:14:24 -0600

    configuration by DatabaseSettingsModel

---
+++ Models/ProductsDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiProducts_MongoDB.Models
+{
+    public class ProductsDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string CollectionName { get; set; } = null!;
+
+    }
+}
